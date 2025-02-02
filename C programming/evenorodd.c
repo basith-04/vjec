70cde661bc0e8c88bd295c3697bbf208fc8364c3
abdul basith p v <basithofficialmail01@gmail.com>
2025-02-02 13:40:45 +0530

    Added sem 02 to repository

---
+++ C programming/evenorodd.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main()
+{
+    float n;
+    printf("Enter the number : \n");
+    scanf("%f",&n);
+    if(n%2==0)
+    {
+        printf("Its even \n");
+    
+    }
+    else
+        printf("Its odd \n");
+}
